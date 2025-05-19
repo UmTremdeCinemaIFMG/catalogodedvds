0118c58a5f3d856103536157a8c2d6b4e5e9576e
UmTremdeCinemaIFMG <umtremdecinema@ifmg.edu.br>
2025-05-19 01:56:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@
                         filmes: parseInt(originalFilm["Filmes"]) || 0,
                         dvd: cleanField(originalFilm["Nome do Programa"]),
                         imageName: cleanField(originalFilm["imageName"]),
-                        classification: parseInt(originalFilm["Classificação Indicativa POR PGM"]) || 0
+                        classification: parseInt(originalFilm["Classificação Indicativa POR PGM"]) || 0,
                         planos_de_aula: originalFilm["planos_de_aula"] || []
                     };
                 }
