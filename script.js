f7d5e1aa5e1bf13ed73e3189ca9cbe124ecd76a4
UmTremdeCinemaIFMG <umtremdecinema@ifmg.edu.br>
2025-05-19 12:59:05 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -519,10 +519,10 @@ function renderTeachingPlans(film) {
                             <h3><i class="fas fa-chalkboard-teacher"></i> Planos de Aula</h3>
                             ${renderTeachingPlans(film)}
                             <a href="https://docs.google.com/forms/d/e/1FAIpQLSdxQz8onMOFjxIqEPpo5v2I4CJdLQ9cN50I7zUhmnBwgUeGIQ/viewform?usp=sharing&ouid=101786859238464224020" target="_blank" class="btn-enviar-plano" style="display:inline-block; margin-top:15px; background:#009a44; color:#fff; padding:10px 18px; border-radius:6px; text-decoration:none; font-weight:500;">
-                                <i class="fas fa-plus"></i> Envie seu plano de aula
+                                <i class="fas fa-plus"></i> Envie um plano de aula
                             </a>
                             <p style="font-size: 0.95em; color: #666; margin-top: 6px;">
-                                Você pode colaborar enviando um plano de aula seu para este filme. Ao clicar, você será direcionado a um formulário.
+                                Você pode colaborar enviando um plano de aula para este filme. Ao clicar, você será direcionado a um formulário.
                             </p>
                         </div>
                         
