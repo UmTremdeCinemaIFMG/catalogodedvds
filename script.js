fb23af58c1bd9eb5d3eaa66d2da4348fc19e01f3
UmTremdeCinemaIFMG <umtremdecinema@ifmg.edu.br>
2025-05-19 20:23:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -621,7 +621,7 @@ function renderTeachingPlans(film) {
                 });
 
             /* ==========================================
-               FUNÇÕES DE PAGINAÇÃO
+               10. FUNÇÕES DE PAGINAÇÃO
                ========================================== */
             
                 // RENDERIZA A PAGINAÇÃO
@@ -723,3 +723,29 @@ function renderTeachingPlans(film) {
                     });
                     pagination.appendChild(nextButton);
                 }
+
+/* ==========================================
+   12. FUNCIONALIDADES DO FALE CONOSCO
+   ========================================== */
+
+// Elementos do DOM para o Fale Conosco
+const modalFaleConosco = document.getElementById("modalFaleConosco");
+const btnFaleConosco = document.getElementById("btnFaleConosco");
+const spanCloseFeedback = modalFaleConosco.querySelector(".close");
+
+// Abre o modal do Fale Conosco
+btnFaleConosco.onclick = function() {
+    modalFaleConosco.style.display = "block";
+}
+
+// Fecha o modal do Fale Conosco ao clicar no X
+spanCloseFeedback.onclick = function() {
+    modalFaleConosco.style.display = "none";
+}
+
+// Fecha o modal do Fale Conosco ao clicar fora dele
+window.addEventListener('click', function(event) {
+    if (event.target == modalFaleConosco) {
+        modalFaleConosco.style.display = "none";
+    }
+});
