682dd685ba1f6f0b1d266d05b788a29e8d963af5
TiagoSilvaIFMG <tiago.silva@ifmg.edu.br>
2025-06-02 21:32:56 -0300

    Update filme.js

--- filme.js
+++ filme.js
@@ -183,9 +183,7 @@ function transformFilmData(originalFilm) {
 
 // FUNÇÃO PARA RENDERIZAR DADOS DO FILME
 function renderFilmData(film) {
-
-
-    
+   
     const filmContainer = document.getElementById('filmeContainer');
     if (!filmContainer) {
         console.error("Container do filme não encontrado");
@@ -201,32 +199,7 @@ function renderFilmData(film) {
     const themes = createThemesList(film);
     const hasThemes = themes.length > 0;
 
-        // ODS
-if (film.ods && film.ods.length > 0) {
-    filmContent += `
-    <div class="filme-section">
-        <h3><i class="fas fa-globe-americas"></i> Objetivos de Desenvolvimento Sustentável</h3>
-        <div class="ods-container">
-            ${film.ods.map(ods => {
-                const odsNumber = ods.match(/\d+/); // Extrai o número do ODS
-                if (odsNumber) {
-                    return `
-                        <a href="https://brasil.un.org/pt-br/sdgs/${odsNumber[0]}" 
-                           target="_blank" 
-                           class="ods-icon" 
-                           title="ODS ${ods}">
-                            <img src="https://brasil.un.org/sites/default/files/styles/large/public/2020-09/E-WEB-Goal-${odsNumber[0]}.png" 
-                                 alt="Ícone do ODS ${ods}" 
-                                 loading="lazy">
-                        </a>
-                    `;
-                }
-                return '';
-            }).join('')}
-        </div>
-    </div>
-    `;
-}
+ 
     
     // INFORMAÇÕES ADICIONAIS
     const hasAdditionalInfo = film.audiodescricao || film.closedCaption || film.website || 
@@ -285,6 +258,33 @@ if (film.ods && film.ods.length > 0) {
         </div>
         `;
     }
+
+           // ODS
+if (film.ods && film.ods.length > 0) {
+    filmContent += `
+    <div class="filme-section">
+        <h3><i class="fas fa-globe-americas"></i> Objetivos de Desenvolvimento Sustentável</h3>
+        <div class="ods-container">
+            ${film.ods.map(ods => {
+                const odsNumber = ods.match(/\d+/); // Extrai o número do ODS
+                if (odsNumber) {
+                    return `
+                        <a href="https://brasil.un.org/pt-br/sdgs/${odsNumber[0]}" 
+                           target="_blank" 
+                           class="ods-icon" 
+                           title="ODS ${ods}">
+                            <img src="https://brasil.un.org/sites/default/files/styles/large/public/2020-09/E-WEB-Goal-${odsNumber[0]}.png" 
+                                 alt="Ícone do ODS ${ods}" 
+                                 loading="lazy">
+                        </a>
+                    `;
+                }
+                return '';
+            }).join('')}
+        </div>
+    </div>
+    `;
+}
     
     // TEMAS
     if (hasThemes) {
