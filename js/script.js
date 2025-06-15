2d08cd04193322e7ffda9a019c2185babfcd2030
UmTremdeCinemaIFMG <umtremdecinema@ifmg.edu.br>
2025-06-15 09:15:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -187,7 +187,7 @@ function filterAndRenderFilms() {
                 (selectedAccessibility === 'audiodescricao' && film.audiodescricao) ||
                 (selectedAccessibility === 'closed_caption' && film.closedCaption) ||
                 (selectedAccessibility === 'trailer' && film.trailer && film.trailer.trim() !== '') ||
-               (selectedAccessibility === 'PGM' && film.PGM) ||
+               (selectedAccessibility === 'pgm' && film.pgm) ||
                 (selectedAccessibility === 'material_outros' && film.materialOutros && film.materialOutros.length > 0) ||
                 (selectedAccessibility === 'assistir_online' && film.assistirOnline && film.assistirOnline.trim() !== '')
             );
